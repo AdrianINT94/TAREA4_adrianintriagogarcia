ee6ae349febf9ca327bc68802f80a92baa02486b
AdrianINT94 <wkj62135@educastur.es>
2026-03-25 23:44:31 +0100

    Delete TAREA3_adrianintriagogarcia directory

--- TAREA3_adrianintriagogarcia/src/main/java/base/repository/CoordinadorRepository.java
+++
@@ -1,9 +0,0 @@
-package base.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import base.model.Coordinador;
-
-public interface CoordinadorRepository extends JpaRepository<Coordinador,Long> {
-
-}
